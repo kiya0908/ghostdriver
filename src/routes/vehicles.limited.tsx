3553663c08923d0e0bd10094f5f1141be7f6435d
kiya0908 <2015189466@qq.com>
2026-08-19 11:00:11 +0800

    seo(routes): make legacy limited route permanent

--- src/routes/vehicles.limited.tsx
+++ src/routes/vehicles.limited.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/vehicles/limited')({
-  beforeLoad: () => { throw redirect({ to: '/cars/limited', replace: true }) },
+  beforeLoad: () => { throw redirect({ to: '/cars/limited', replace: true, statusCode: 301 }) },
 })
